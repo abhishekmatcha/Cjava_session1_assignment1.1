3f40fa62d659c2558fd91f6b22d308bdc94a025c
abhishekmatcha <seshaabhishek@gmail.com>
2017-01-29 23:55:51 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum
+{
+	public static void main(String args[])
+	{
+		System.out.println("enter values of a and b");
+		 int a=Integer.parseInt(System.console().readLine());
+                 int b=Integer.parseInt(System.console().readLine());  
+                int sum=a-(-b);
+		System.out.println(sum);
+	}
+}
